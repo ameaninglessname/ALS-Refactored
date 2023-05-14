0f26427d4c9cea97ea8924bd0c5374f001cdd792
ameaninglessname <ameaninglessname@users.noreply.github.com>
2023-05-14 12:59:27 +0800

    New: add more virtual API to UAlsCameraComponent

--- Source/ALSCamera/Private/AlsCameraComponent.cpp
+++ Source/ALSCamera/Private/AlsCameraComponent.cpp
@@ -207,7 +207,7 @@ void UAlsCameraComponent::TickCamera(const float DeltaTime, bool bAllowLag)
 		}
 	}
 
-	const auto CameraTargetRotation{Character->GetViewRotation()};
+	const auto CameraTargetRotation{GetCameraTargetRotation()};
 
 	const auto PreviousPivotTargetLocation{PivotTargetLocation};
 
@@ -305,7 +305,7 @@ void UAlsCameraComponent::TickCamera(const float DeltaTime, bool bAllowLag)
 
 	// Calculate target camera location.
 
-	const auto CameraTargetLocation{PivotLocation + CalculateCameraOffset()};
+	const auto CameraTargetLocation{GetCameraTargetLocation()};
 
 	// Trace for an object between the camera and character to apply a corrective offset.
 
@@ -328,6 +328,16 @@ void UAlsCameraComponent::TickCamera(const float DeltaTime, bool bAllowLag)
 	}
 
 	CameraFieldOfView = FMath::Clamp(CameraFieldOfView + CalculateFovOffset(), 5.0f, 175.0f);
+}
+
+FRotator UAlsCameraComponent::GetCameraTargetRotation() const
+{
+	return Character->GetViewRotation();
+}
+
+FVector UAlsCameraComponent::GetCameraTargetLocation() const
+{
+	return PivotLocation + CalculateCameraOffset();
 }
 
 FRotator UAlsCameraComponent::CalculateCameraRotation(const FRotator& CameraTargetRotation,
