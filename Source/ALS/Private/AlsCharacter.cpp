53f38c540da7bd336ddb01f92ff76071d877b1b4
ameaninglessname <ameaninglessname@users.noreply.github.com>
2024-02-20 22:10:39 +0800

    New: APIs added to support free look

--- Source/ALS/Private/AlsCharacter.cpp
+++ Source/ALS/Private/AlsCharacter.cpp
@@ -1139,6 +1139,16 @@ FRotator AAlsCharacter::GetViewRotation() const
 	return ViewState.Rotation;
 }
 
+FRotator AAlsCharacter::GetViewRotator() const
+{
+	return Super::GetViewRotation();
+}
+
+bool AAlsCharacter::ShouldSendViewRotationRpc() const
+{
+	return !IsReplicatingMovement();
+}
+
 void AAlsCharacter::SetInputDirection(FVector NewInputDirection)
 {
 	NewInputDirection = NewInputDirection.GetSafeNormal();
@@ -1277,7 +1287,7 @@ void AAlsCharacter::RefreshView(const float DeltaTime)
 			// We can't depend on the view rotation sent by the character movement component
 			// since it's in world space, so in this case we always send it ourselves.
 
-			SetReplicatedViewRotation((MovementBase.Rotation.Inverse() * Super::GetViewRotation().Quaternion()).Rotator(), true);
+			SetReplicatedViewRotation((MovementBase.Rotation.Inverse() * GetViewRotator().Quaternion()).Rotator(), true);
 		}
 	}
 	else
@@ -1287,7 +1297,7 @@ void AAlsCharacter::RefreshView(const float DeltaTime)
 			// The character movement component already sends the view rotation to the
 			// server if movement is replicated, so we don't have to do this ourselves.
 
-			SetReplicatedViewRotation(Super::GetViewRotation().GetNormalized(), !IsReplicatingMovement());
+			SetReplicatedViewRotation(GetViewRotator().GetNormalized(), ShouldSendViewRotationRpc());
 		}
 	}
 
