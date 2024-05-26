e74e259c733247d6136d3810ef426e90e54a882f
ameaninglessname <ameaninglessname@users.noreply.github.com>
2024-05-26 13:37:47 +0800

    Changed: using latest IncludeOrderVersion

--- Source/ALS/ALS.Build.cs
+++ Source/ALS/ALS.Build.cs
@@ -5,7 +5,6 @@ public class ALS : ModuleRules
 	public ALS(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 
 		bEnableNonInlinedGenCppWarnings = true;
 		// UnsafeTypeCastWarningLevel = WarningLevel.Warning;
--- Source/ALSCamera/ALSCamera.Build.cs
+++ Source/ALSCamera/ALSCamera.Build.cs
@@ -5,7 +5,6 @@ public class ALSCamera : ModuleRules
 	public ALSCamera(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 
 		bEnableNonInlinedGenCppWarnings = true;
 		// UnsafeTypeCastWarningLevel = WarningLevel.Warning;
